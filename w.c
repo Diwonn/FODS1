d2da8e8656f326d612823070255b499f3492359f
Diwon Sigdel <diwonsigdel@Sigdel.local>
2022-01-25 14:11:12 +0545

    class and object

---
+++ w.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+int num;
+FILE *fptr;
+fptr = fopen("diwonsigfile.txt","w");
+ if(fptr == NULL) {
+   printf("Error!"); 
+   exit(1); 
+}
+printf("Enter number: ");
+ scanf("%d",&num);
+fprintf(fptr,"%d",num);
+ fclose(fptr);
+ return 0;
+}
